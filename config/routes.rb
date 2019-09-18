434eece945c3c5fe661207b14a0e3e43f457855b
itoyamatakashi <takshi03090619@gmail.com>
2019-09-18 11:07:32 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# ルーティングを追加
 Rails.application.routes.draw do
-  root to: 'messages#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
