27878f5fabc05a0fb95ea6139ceabe30a12c3dfb
itoyamatakashi <takshi03090619@gmail.com>
2019-09-25 17:28:01 +0900

    自動更新機能を実装

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,10 @@
 class Api::MessagesController < ApplicationController
   def index
-    group = Group.find(params[:group_id])
+    #グループの情報をインスタンス変数@groupに保存 これを元にDBからグループを取得する
+    @group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
-    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
+    # ajaxで送られてくる最後のメッセージのid番号を変数に代入
+    @messages = @group.messages.includes(:user).where("id > #{last_message_id}")
+    # includesはN+1問題対策
   end
 end
