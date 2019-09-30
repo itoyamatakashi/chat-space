d12bc527d7a8c4d4015919447633b9b1f9bdc4a5
itoyamatakashi <takshi03090619@gmail.com>
2019-09-30 16:51:16 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.1"
+lock "3.1"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:itoyamatakashi/chat-space.git"
+set :repo_url, "git@github.com:itoyamatakashi/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],
-keys: ['/.ssh/chat-space.pem']
+                  keys: ['/.ssh/chat-space.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
