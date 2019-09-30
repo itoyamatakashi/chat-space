ea7badffd1b261c8efc84a11599c7d64d11b1e03
itoyamatakashi <takshi03090619@gmail.com>
2019-09-30 17:03:24 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "3.1"
+lock "3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com:itoyamatakashi/chat-space.git"
+set :repo_url,  "git@github.com:itoyamatakashi/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
